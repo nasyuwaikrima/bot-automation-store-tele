d66e74d8529039ad5908ec27ea7bea71d52c4204
nasyuwaikrima <nasyuwa648@gmail.com>
2026-05-17 06:07:19 +0000

    feat: add example config

---
+++ config.example.js
@@ -0,0 +1,8 @@
+module.exports = {
+  BOT_TOKEN: '', // Ganti dengan token asli dari BotFather
+  REQUIRED_CHANNEL: '', 
+  START_IMAGE_URL: 'https://i.ibb.co.com/27mFtKfX/eaade851c59be96263fdca20393c1672.jpg', // Gambar sambutan untuk /start
+  cashiApikey: "",
+  AUTO_BACKUP: 1,
+  OWNER_ID:  // Ganti dengan ID Telegram kamu
+};
